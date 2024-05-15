acdd15f2f89d6a58e5ade43d4c80bd1d62fe8420
t-mio <takasshhi21@gmail.com>
2024-05-15 20:49:18 +0200

    設定ファイルの変更

---
+++ config/settings/dev.py
@@ -0,0 +1,20 @@
+from .base import *
+from decouple import config, Csv
+
+
+# 環境変数の読み込み
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='localhost', cast=Csv())
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
+}
---
+++ config/settings/prod.py
@@ -0,0 +1,18 @@
+from .base import *
+from decouple import config, Csv
+
+
+# 環境変数の読み込み
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='localhost', cast=Csv())
+
+
+# 環境変数からデータベースURLを取得
+DATABASE_URL = config('DATABASE_URL')
+
+
+# DATABASES設定を作成
+DATABASES = {
+    'default': dj_database_url.parse(DATABASE_URL)
+}
